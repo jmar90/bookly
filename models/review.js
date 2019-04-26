527958135a4b612e0553462f56b0974765850f2a
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-04-26 13:51:03 -0400

    Added moment.js to display time since created

--- models/review.js
+++ models/review.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // SCHEMA SET UP
 const reviewSchema = new mongoose.Schema({
 	text: String,
+	createdAt: { type: Date, default: Date.now },
 	author: {
 		id: {
 			type: mongoose.Schema.Types.ObjectId,
